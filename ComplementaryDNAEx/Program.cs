4107a939704ebffa0a0285829138db0004acbf96
Gaël Louage <gaellouage@hotmail.com>
2025-07-01 12:49:48 +0200

    Complementary DNA Exercise

---
+++ ComplementaryDNAEx/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(MakeComplement("ATTGC"));
+    }
+    public static string MakeComplement(string dna)
+    {
+        if (string.IsNullOrWhiteSpace(dna)) return "";
+
+        dna =  dna.ToUpper();
+
+        var sb = new StringBuilder();
+
+        foreach (var c in dna)
+        {
+            sb.Append(SwitchChar(c));
+        } 
+
+        return sb.ToString();
+    }
+
+    private static string SwitchChar(char c)
+    {
+        return c switch
+        {
+            'A' => "T",
+            'T' => "A",
+            'G' => "C",
+            'C' => "G",
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
